10c2444b97cf2a964bdaed4344047712392508bd
akiyoshi_okumura <akiyoshia.m.1231@gmail.com>
2019-09-30 17:02:25 +0900

    add post_model_validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,13 +5,34 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-
   def new
     @post = Post.new
   end
-  
   def create
-    post = Post.create(post_params)
+    post = Post.new(post_params)
+    if post.save
+      flash[:notice]= "投稿を作成しました"
+      redirect_to action: :index
+    else
+      render action: :new
+    end
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      flash[:notice]= "投稿を編集しました"
+      redirect_to action: :index
+    else
+      render action: :edit
+    end
+  end
+  def destroy
+    post = Post.find(params[:id])
+    post.delete
+    flash[:notice]= "投稿を削除しました"
     redirect_to action: :index
   end
 
