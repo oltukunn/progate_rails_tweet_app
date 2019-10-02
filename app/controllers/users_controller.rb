43b57e4978e291b6cb0e7bb65dd3c840b7e3264b
akiyoshi_okumura <akiyoshia.m.1231@gmail.com>
2019-10-02 15:23:13 +0900

    add picture upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,18 +22,21 @@ class UsersController < ApplicationController
   end
   def update
     user = User.find(params[:id])
+    if user.image_name
+      user.image_name = "#{user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{user.image_name}", image.read)
+    end  
     if user.update(user_params)
       flash[:notice]="編集が完了しました"
       redirect_to user_path(user)
     else
       render action: :edit
     end
-
   end
-
 
     private
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name,:email,:image_name)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_062224) do
+ActiveRecord::Schema.define(version: 2019_10_01_103905) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_062224) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
   end
 
 end
