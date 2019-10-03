f52d76e8db7a18b64d3249c3a89c88c2f398ad0b
akiyoshi_okumura <akiyoshia.m.1231@gmail.com>
2019-10-03 19:13:29 +0900

    dosen't work create_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,6 +62,6 @@ class UsersController < ApplicationController
   # ここからストロングパラメータ
     private
     def user_params
-      params.require(:user).permit(:name,:email)
+      params.require(:user).permit(:name,:email,:password)
     end
 end
